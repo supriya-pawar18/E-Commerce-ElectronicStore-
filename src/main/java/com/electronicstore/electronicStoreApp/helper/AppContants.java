467514b59a396adb62d094ce00f7f1e211907868
Supriya_Pawar18 <suppawar6@gmail.com>
2023-12-05 15:39:23 +0530

    Adding pagination class

--- src/main/java/com/electronicstore/electronicStoreApp/helper/AppContants.java
+++ src/main/java/com/electronicstore/electronicStoreApp/helper/AppContants.java
@@ -1,21 +1,10 @@
 package com.electronicstore.electronicStoreApp.helper;
 
 public class AppContants {
-    public static final String PAGE_NUMBER="0";
 
-    public static final String PAGE_SIZE="5";
+    public static final String NOT_FOUND="Resource not found with id";
 
-    public static final String SORT_BY="postId";
-
-    public static final String SORT_DIR="asc";
-
-    public static final String NOT_FOUND="Resource not found with user user id";
-
-    public static final String USER_DELETED="Resource not found with user id";
-    public static final String CATEGORY_DELETED="Resource not found with category id";
-
-
-
+    public static final String USER_DELETED="Resource not found with  id";
 
 
 }
