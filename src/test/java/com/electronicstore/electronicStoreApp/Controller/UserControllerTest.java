6508a46867da9db4530dd15d4719964742eea815
Supriya_Pawar18 <suppawar6@gmail.com>
2023-12-07 16:12:41 +0530

    Test cases of updateUser of userController of user

--- src/test/java/com/electronicstore/electronicStoreApp/Controller/UserControllerTest.java
+++ src/test/java/com/electronicstore/electronicStoreApp/Controller/UserControllerTest.java
@@ -1,6 +1,7 @@
 package com.electronicstore.electronicStoreApp.Controller;
 
 import com.electronicstore.electronicStoreApp.ServiceI.UserServiceI;
+import com.electronicstore.electronicStoreApp.dto.PageableResponse;
 import com.electronicstore.electronicStoreApp.dto.UserDto;
 import com.electronicstore.electronicStoreApp.entites.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import javax.management.relation.Role;
+import java.util.Arrays;
 import java.util.Set;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -79,12 +81,13 @@ public class UserControllerTest {
         }
     }
 
+    @Test
     public void updateUserTest() throws Exception {
         String id="12";
         UserDto userDto=this.modelMapper.map(user,UserDto.class);
         Mockito.when(userServiceI.updateUser(Mockito.any(),Mockito.anyString())).thenReturn(userDto);
 
-        this.mockMvc.perform(MockMvcRequestBuilders.put("/user/")
+        this.mockMvc.perform(MockMvcRequestBuilders.put("/user/" + id)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(convertObjectToJsonString(user))
                         .accept(MediaType.APPLICATION_JSON)
@@ -93,5 +96,32 @@ public class UserControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name").exists());
     }
+
+    @Test
+    public void getAllUserTest() throws Exception {
+        UserDto userDto= UserDto.builder().name("vivek").about("This is update data").gender("Male").imgname("abc.png").build();
+        UserDto userDto1= UserDto.builder().name("Nilu").about("This is update data").gender("Female").imgname("gef.png").build();
+        UserDto userDto2= UserDto.builder().name("Ashu").about("This is update data").gender("Female").imgname("avc.png").build();
+        UserDto userDto3= UserDto.builder().name("Siya").about("This is update data").gender("Female").imgname("si.png").build();
+
+        PageableResponse<UserDto> pageableResponse=new PageableResponse<>();
+        pageableResponse.setContent(Arrays.asList(userDto,userDto1,userDto2,userDto3));
+
+        pageableResponse.setLastPage(false);
+        pageableResponse.setPageSize(10);
+        pageableResponse.setPageNumber(20);
+        pageableResponse.setTotalElements(100);
+
+        Mockito.when(userServiceI.getAllUsers(Mockito.anyInt(),Mockito.anyInt(),Mockito.anyString(),Mockito.anyString())).thenReturn(pageableResponse);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/user/")
+                      .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+
 
 }
