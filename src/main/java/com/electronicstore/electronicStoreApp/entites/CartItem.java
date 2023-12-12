fe5449c85c64c5167cf9f4be73f4dc1b1bdc63c8
Supriya_Pawar18 <suppawar6@gmail.com>
2023-12-12 21:50:08 +0530

    Cart item Table created

--- src/main/java/com/electronicstore/electronicStoreApp/entites/CartItem.java
+++ src/main/java/com/electronicstore/electronicStoreApp/entites/CartItem.java
@@ -1,4 +1,27 @@
 package com.electronicstore.electronicStoreApp.entites;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cartItemId;
+
+    @OneToOne
+    private Product product;
+
+    private int quantity;
+
+    private int totalPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
 }
