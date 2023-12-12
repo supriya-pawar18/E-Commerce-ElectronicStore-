1cfb915f52402ef37f5bf52122ba88149ee309c3
Supriya_Pawar18 <suppawar6@gmail.com>
2023-12-12 12:58:42 +0530

    Test case of product for delete and getAll

--- src/test/java/com/electronicstore/electronicStoreApp/Service/ProductServiceTest.java
+++ src/test/java/com/electronicstore/electronicStoreApp/Service/ProductServiceTest.java
@@ -16,6 +16,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 public class ProductServiceTest {
@@ -54,4 +62,76 @@ public class ProductServiceTest {
 
         Assertions.assertEquals("Samsumg", product1.getTitle());
     }
+
+    @Test
+    public void updateProductTest() {
+
+        String productId = "bacdefcgf";
+
+         ProductDto productDto = ProductDto.builder()
+                .title("Samsumg")
+                .description("The details related to info")
+                .price(50000)
+                .discountedPrice(40000)
+                .quantity(40)
+                .live(false)
+                .stock(false)
+                .build();
+
+        Mockito.when(productRepo.findById(Mockito.anyString())).thenReturn(Optional.of(product));
+        Mockito.when(productRepo.save(Mockito.any())).thenReturn(product);
+
+        ProductDto updateProd = productService.update(productDto, productId);
+        System.out.println(updateProd.getTitle());
+        Assertions.assertNotNull(productDto);
+
+        Assertions.assertEquals(productDto.getTitle(), updateProd.getTitle(), "Title is not equal");
+
+    }
+
+    @Test
+    public void deleteProdTest() {
+
+        String productId = "abcdefghijklm";
+
+        Mockito.when(productRepo.findById(productId)).thenReturn(Optional.of(product));
+
+        productService.delete(productId);
+
+        Mockito.verify(productRepo, Mockito.times(1)).delete(product);
+
+    }
+    @Test
+    public void getAllProdTest(){
+
+        Product product1 = Product.builder()
+                .title("Samsumg")
+                .description("The details related to info")
+                .price(50000)
+                .discountedPrice(40000)
+                .quantity(40)
+                .live(false)
+                .stock(false)
+                .build();
+
+        Product product2 = Product.builder()
+                .title("IPhone")
+                .description("The details related to info")
+                .price(100000)
+                .discountedPrice(90000)
+                .quantity(50)
+                .live(false)
+                .stock(false)
+                .build();
+
+        List<Product> productList = Arrays.asList(product1, product2);
+
+        Page<Product> page = new PageImpl<>(productList);
+
+        Mockito.when(productRepo.findAll((Pageable) Mockito.any())).thenReturn(page);
+
+        Sort sort = Sort.by("title").ascending();
+
+    }
+
 }
