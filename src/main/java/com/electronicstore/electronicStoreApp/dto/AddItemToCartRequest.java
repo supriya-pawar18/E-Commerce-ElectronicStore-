b6e40e04e9f25a8f4b4a91e72b3a5c67f9542cc0
Supriya_Pawar18 <suppawar6@gmail.com>
2023-12-13 12:46:21 +0530

    Creating Add Item in Dto class

---
+++ src/main/java/com/electronicstore/electronicStoreApp/dto/AddItemToCartRequest.java
@@ -0,0 +1,18 @@
+package com.electronicstore.electronicStoreApp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class AddItemToCartRequest {
+
+    private int productId;
+
+    private int quantity;
+
+}
