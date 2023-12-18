07ddcce82d4be7253f08a13dd5ddaa5c22974e68
Supriya_Pawar18 <suppawar6@gmail.com>
2023-12-18 14:19:22 +0530

    Adding Logger to cartController

--- src/main/java/com/electronicstore/electronicStoreApp/Controller/CartController.java
+++ src/main/java/com/electronicstore/electronicStoreApp/Controller/CartController.java
@@ -1,10 +1,13 @@
 package com.electronicstore.electronicStoreApp.Controller;
 
 import com.electronicstore.electronicStoreApp.ServiceI.CartService;
+import com.electronicstore.electronicStoreApp.ServiceImpl.CategoryServiceImpl;
 import com.electronicstore.electronicStoreApp.dto.AddItemToCartRequest;
 import com.electronicstore.electronicStoreApp.dto.ApiResponse;
 import com.electronicstore.electronicStoreApp.dto.CartDto;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -18,37 +21,51 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
+    private Logger logger= LoggerFactory.getLogger(CartController.class);
+
+
     @PostMapping("/{id}")
     public ResponseEntity<CartDto> addItemToCart(@PathVariable String id,@RequestBody AddItemToCartRequest request) {
+        logger.info("Initiating dao request for add item to cart with user id {}:",id);
         CartDto cartDto = cartService.addItemToCart(id, request);
+        logger.info("Completed dao call to add item into cart with userId:{}", id);
+
         return new ResponseEntity<>(cartDto, HttpStatus.OK);
     }
 
     @GetMapping("/{id}/items/{itemId}")
     public ResponseEntity<ApiResponse> removeItemFromCart( @PathVariable String id,@PathVariable int itemId){
+        logger.info("Initiating dao request for remove item from cart with user id {}:",id);
         cartService.removeItemFromCart(id,itemId);
         ApiResponse apiResponse = ApiResponse.builder()
                 .message("Item is removed")
                 .success(true)
                 .status(HttpStatus.OK)
                 .build();
+
+        logger.info("Completed dao call to remove item from cart with userId:{}", id);
         return new ResponseEntity<>(apiResponse,HttpStatus.OK);
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponse> clearCart(@PathVariable String id){
-
+        logger.info("Initiating dao request for clear item from cart with user id {}:",id);
         cartService.clearCart(id);
         ApiResponse apiResponse = ApiResponse.builder()
                 .message("Cart is not blank !!")
                 .success(true)
                 .status(HttpStatus.OK)
                 .build();
+
+        logger.info("Completed dao call to clear item from cart with userId:{}", id);
         return new ResponseEntity<>(apiResponse,HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<CartDto> getCartByUser(@PathVariable String id) {
+        logger.info("Initiating dao request for get cart item by user from cart with user id {}:",id);
         CartDto cartDto = cartService.getCartByUser(id);
+
+        logger.info("Completed dao call to get cart item by user from cart with userId:{}", id);
         return new ResponseEntity<>(cartDto, HttpStatus.OK);
     }
     }
