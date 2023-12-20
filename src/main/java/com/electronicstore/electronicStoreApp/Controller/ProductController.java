41863fa425b32d013ca824cb6ffdd139e5129818
Supriya_Pawar18 <suppawar6@gmail.com>
2023-12-20 11:47:41 +0530

    testing addtocart method of cart module

--- src/main/java/com/electronicstore/electronicStoreApp/Controller/ProductController.java
+++ src/main/java/com/electronicstore/electronicStoreApp/Controller/ProductController.java
@@ -89,7 +89,7 @@ public class ProductController {
      */
     @GetMapping("/")
     public ResponseEntity<PageableResponse<ProductDto>> getAll(
-            @RequestParam(value = "pageNumber",defaultValue = "0",required = false) int pageNumber,
+            @RequestParam(value = "pageNumber",defaultValue = "1",required = false) int pageNumber,
             @RequestParam(value = "pageSize",defaultValue = "10",required = false) int pageSize,
             @RequestParam(value = "sortBy",defaultValue = "title",required = false) String sortBy,
             @RequestParam(value = "sortDir",defaultValue = "asc",required = false) String sortDir)
