7bbeb1a294fb147129f8e87f54b1c7f49d751a7c
Supriya_Pawar18 <suppawar6@gmail.com>
2024-01-03 19:24:35 +0530

    Test case for getUserByOrder method in ordercontroller Test

--- src/test/java/com/electronicstore/electronicStoreApp/Controller/OrderControllerTest.java
+++ src/test/java/com/electronicstore/electronicStoreApp/Controller/OrderControllerTest.java
@@ -21,6 +21,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -72,22 +79,24 @@ public class OrderControllerTest {
     public void createOrderTest() throws Exception {
 
         CreateOrderRequest orderRequest = CreateOrderRequest.builder()
-                .cartId("12344")
-                .billingAddress("Pune")
-                .billingPhone("4859379")
-                .billingName("ash")
+                .cartId("123")
+                .id("1234")
+                .orderStatus("Pending")
+                .paymentStatus("Not_Paid")
+                .billingAddress("Nashik")
+                .billingPhone("000")
                 .build();
 
-        String id = "1234";
-
         OrderDto orderDto = this.modelMapper.map(order, OrderDto.class);
-        Mockito.when(orderService.createOrder( Mockito.any())).thenReturn(orderDto);
-        mockMvc.perform(MockMvcRequestBuilders.post("/order/" )
+
+        Mockito.when(orderService.createOrder(Mockito.any())).thenReturn(orderDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/order/create")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(convertObjectToJsonString(orderRequest))
                         .accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andDo(print());
     }
 
     private String convertObjectToJsonString(Object orderRequest) {
@@ -105,7 +114,35 @@ public class OrderControllerTest {
         String orderId="abc";
 
         Mockito.doNothing().when(orderService).removeOrder(orderId);
-        mockMvc.perform(MockMvcRequestBuilders.delete("/remove/{orderId}", orderId))
+        mockMvc.perform(MockMvcRequestBuilders.delete("/order/remove/", orderId))
+                .andDo(print());
+              //  .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getOrderOfUserTest() throws Exception {
+        String id = "abcd";
+
+        Order order2 = Order.builder().orderId("poer").orderStatus("PENDING").paymentStatus("NOTPAID").orderAmount(200)
+                .billingName("Supriya").billingAddress("Pune").billingPhone("987456321").orderDate(new Date())
+                .user(user).build();
+
+        Order order3 = Order.builder().orderId("stur").orderStatus("PENDING").paymentStatus("NOTPAID").orderAmount(200)
+                .billingName("vvk").billingAddress("Pune").billingPhone("987456321").orderDate(new Date())
+                .user(user).build();
+
+        List<Order> list = new ArrayList<>();
+        list.add(order2);
+        list.add(order3);
+
+        List<OrderDto> dtos = list.stream().map(i -> modelMapper.map(i, OrderDto.class)).collect(Collectors.toList());
+        Mockito.when(orderService.getOrdersOfUser(Mockito.anyString())).thenReturn(dtos);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/order/user/" + id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+
+                )
                 .andDo(print())
                 .andExpect(status().isOk());
     }
