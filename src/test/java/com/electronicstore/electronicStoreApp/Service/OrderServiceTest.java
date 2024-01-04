f68e40be0f08dc02a552e229df956e70c2e0a51a
Supriya_Pawar18 <suppawar6@gmail.com>
2024-01-04 12:52:42 +0530

    Test case of getOrdersTest method in orderService class

--- src/test/java/com/electronicstore/electronicStoreApp/Service/OrderServiceTest.java
+++ src/test/java/com/electronicstore/electronicStoreApp/Service/OrderServiceTest.java
@@ -5,6 +5,7 @@ import com.electronicstore.electronicStoreApp.ServiceI.OrderService;
 import com.electronicstore.electronicStoreApp.dto.CartDto;
 import com.electronicstore.electronicStoreApp.dto.CreateOrderRequest;
 import com.electronicstore.electronicStoreApp.dto.OrderDto;
+import com.electronicstore.electronicStoreApp.dto.PageableResponse;
 import com.electronicstore.electronicStoreApp.entites.*;
 import com.electronicstore.electronicStoreApp.repository.*;
 import org.junit.jupiter.api.Assertions;
@@ -16,11 +17,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.*;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -128,7 +127,7 @@ public class OrderServiceTest {
     @Test
     public void getOrderOfUserTest() {
         Order order2 = Order.builder().orderId("abd").orderStatus("PENDING").paymentStatus("NOTPAID").orderAmount(200)
-                .billingName("Supriya").billingAddress("Pune").billingPhone("987456321").orderDate(new Date())
+                .billingName("Supriya").billingAddress("Nashik").billingPhone("987456321").orderDate(new Date())
                 .user(user).orderItems(orderItems).build();
 
         Order order3 = Order.builder().orderId("acd").orderStatus("PENDING").paymentStatus("NOTPAID").orderAmount(200)
@@ -147,6 +146,27 @@ public class OrderServiceTest {
         List<OrderDto> orderOfUser = orderService.getOrdersOfUser(id);
 
         Assertions.assertEquals(orderList.size(), orderOfUser.size());
+
+    }
+
+    @Test
+    void getOrdersTest() {
+        Order order2 = Order.builder().orderId("abcd").orderStatus("PENDING").paymentStatus("NOTPAID").orderAmount(200)
+                .billingName("Supriya").billingAddress("Pune").billingPhone("893489234").orderDate(new Date())
+                .user(user).orderItems(orderItems).build();
+
+        Order order3 = Order.builder().orderId("abcd").orderStatus("PENDING").paymentStatus("NOTPAID").orderAmount(200)
+                .billingName("AShu").billingAddress("Nashik").billingPhone("9763239803").orderDate(new Date())
+                .user(user).orderItems(orderItems).build();
+        List<Order> list = Arrays.asList(order, order2, order3);
+        Page<Order> orderlist = new PageImpl<>(list);
+        Mockito.when(orderRepo.findAll((Pageable) Mockito.any())).thenReturn(orderlist);
+        Sort sort = Sort.by("name").ascending();
+
+        PageRequest request = PageRequest.of(1, 2, sort);
+        PageableResponse<OrderDto> allProduct = orderService.getOrders("1", 1, 10, "asc","a");
+      //  Assertions.assertEquals(3, allProduct.getContent().size());
+
 
     }
 
