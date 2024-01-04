59e024550ef4e249ff5b97622d8572083db35237
Supriya_Pawar18 <suppawar6@gmail.com>
2024-01-04 17:23:45 +0530

    Creaing other methods in Jwt Helper class

--- src/main/java/com/electronicstore/electronicStoreApp/Security/JwtAuthicationEntryPoint.java
+++ src/main/java/com/electronicstore/electronicStoreApp/Security/JwtAuthicationEntryPoint.java
@@ -8,11 +8,16 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Component
 public class JwtAuthicationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        writer.println("Access Denied !!" + authException.getMessage());
 
     }
 }
