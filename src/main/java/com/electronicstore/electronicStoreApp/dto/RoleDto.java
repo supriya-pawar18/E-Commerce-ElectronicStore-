128a1e58c226e0f9417ae8d8b9942b3ac1f1ac2b
Supriya_Pawar18 <suppawar6@gmail.com>
2024-01-06 14:01:54 +0530

    Creating roleDto class for authentication

---
+++ src/main/java/com/electronicstore/electronicStoreApp/dto/RoleDto.java
@@ -0,0 +1,15 @@
+package com.electronicstore.electronicStoreApp.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleDto {
+
+    private String roleId;
+
+    private String roleName;
+}
